16298cb1efde954dac63ee0b40790a6bf9e3fe3e
Timur Anvartdinov <t.anvartdinov@life-stream.tv>
2026-03-01 00:06:28 +0300

    feat: fix import

--- src/main/kotlin/domain/result/Result.kt
+++ src/main/kotlin/domain/result/Result.kt
@@ -1,6 +1,8 @@
 package domain.result
 
+import domain.error.DomainError
+
 sealed interface Result<out T> {
     data class Ok<T>(val value: T) : Result<T>
-    data class Err(val error: domain.error.DomainError) : Result<Nothing>
+    data class Err(val error: DomainError) : Result<Nothing>
 }
